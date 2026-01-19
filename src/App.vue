853855860dce4cb396a24387af79a40849f0cb44
Tobby <tobby2588@gmail.com>
2026-01-19 15:55:50 +0800

    Fresh deploy

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    
+    <div class="fixed top-0 left-0 w-full z-50">
+      <Navbar />
+    </div>
+
+    <main class="flex-1 pt-16">
+      <router-view />
+    </main>
+
+    <Footer />
+    
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style>
+/* 全域樣式設定，確保排版正常 */
+body {
+  margin: 0;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+}
+</style>
